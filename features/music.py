e198ed5add8047a0499742972aaf1c7ea760562a
SunidhiSingh0604 <sunidhisingh060402@gmail.com>
2026-04-30 22:10:24 +0530

    Add music playback feature to play songs on YouTube

--- features/music.py
+++ features/music.py
@@ -1,1 +1,12 @@
+import webbrowser
+import pywhatkit
+from voice.tts import say
 
+def play_song_online(song_name):
+    try:
+        say(f"Playing {song_name} on YouTube")
+        pywhatkit.playonyt(song_name)
+    except:
+        search_url = f"https://www.youtube.com/results?search_query={song_name}"
+        webbrowser.open(search_url)
+        say(f"Playing {song_name} on YouTube")
